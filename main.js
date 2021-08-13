bd553071216a6c7f9eb596faec91686da1fc3ac6
EduAlm83 <87372233+EduAlm83@users.noreply.github.com>
2021-08-13 10:53:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,17 +38,19 @@ jsArrow.onclick = function () {
 }
 
 //swiper-slider
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
